9d4e4d03dd03b72941f1a15eabb64b826ab6790b
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-19 20:29:17 +0000

    fixed cors regexes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,44 +3,16 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/hw-logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
-
-
 
 const NavBar = () => {
-  const currentUser = useCurrentUser();
-
-  const loggedInIcons = <>{currentUser?.username}</>;
-  const loggedOutIcons = (
-    <>
-      <NavLink
-        className={styles.NavLink}
-        activeClassName={styles.Active}
-        to="/signin"
-      >
-        <i className="fas fa-sign-in-alt"></i>Sign in
-      </NavLink>
-      <NavLink
-        to="/signup"
-        className={styles.NavLink}
-        activeClassName={styles.Active}
-      >
-        <i className="fas fa-user-plus"></i>Sign up
-      </NavLink>
-    </>
-  );
-
-
-
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-      <NavLink to="/">
+        <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto text-left">
@@ -52,8 +24,20 @@ const NavBar = () => {
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
-
-            {currentUser ? loggedInIcons : loggedOutIcons}
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
