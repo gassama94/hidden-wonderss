1731501484f6a82bf5e6a9cb61dd1a05c23f3fd7
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-19 14:57:32 +0000

    added HandleChnage and HandhleSubmit and error alerts for SignUpForm.js

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,14 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import nangyuanIsland from '../../assets/nangyuan-island.jpg';
 
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert,} from "react-bootstrap";
+import axios from "axios";
+
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      console.log(err.response);
+      setErrors(err.response?.data);
+    }
+  };
+
+
   return (
     
     <Row className={styles.Row}>
@@ -23,25 +55,54 @@ const SignUpForm = () => {
           <h1 className={styles.Header}>sign up</h1>
 
             {/* add your form here */}
-            <Form>
+            <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="username">
         <Form.Label className="d-none">Username</Form.Label>
-        <Form.Control className={styles.Input} type="text" placeholder="username" name="username" />
+        <Form.Control className={styles.Input} type="text" placeholder="username" name="username" value={username} onChange={handleChange} />
       </Form.Group>
+
+      {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
 
       <Form.Group className="mb-3" controlId="password1">
         <Form.Label className="d-none">Password</Form.Label>
-        <Form.Control className={styles.Input} type="password" placeholder="password" name="password1" />
+        <Form.Control className={styles.Input} type="password" placeholder="password" name="password1" value={password1} onChange={handleChange} />
       </Form.Group>
+
+      {errors.password1?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
 
       <Form.Group className="mb-3" controlId="password2">
         <Form.Label className="d-none">Confirm Password</Form.Label>
-        <Form.Control className={styles.Input} type="password" placeholder="confirm password" name="password2" />
+        <Form.Control className={styles.Input} type="password" placeholder="confirm password" name="password2" value={password2} onChange={handleChange} />
       </Form.Group>
+
+
+      {errors.password2?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
       <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
         Sign Up
       </Button>
+
+      {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+
+
     </Form>
 
         </Container>
